e381d00fccb3c19fa3ef40db1b61743a9976e545
GustavoCruz <>
2025-02-23 20:05:22 +0100

    test7

--- custom/onscreen_keyboard.sh
+++ custom/onscreen_keyboard.sh
@@ -4,14 +4,14 @@ CONFIG_FILE="../config.json"
 script_name=$(basename "$0" .sh)  # Obtener el nombre del script sin la extensión .sh
 
 # Leer la configuración actual
-#value=$(jq -r ".${script_name}.value" "$CONFIG_FILE")
-#
-#if [[ "$value" == "enabled" ]]; then
-#    echo "[SYS]: Enabling on-screen keyboard..."
-#    #sudo apt install -y onboard  # Instala el teclado en pantalla
-#else
-#    echo "[SYS]: Disabling on-screen keyboard..."
-#    #sudo apt remove -y onboard  # Desinstala el teclado en pantalla
-#fi
+value=$(jq -r ".${script_name}.value" "$CONFIG_FILE")
 
-echo $script_name
+if [[ "$value" == "enabled" ]]; then
+    echo "[SYS]: Enabling on-screen keyboard..."
+    #sudo apt install -y onboard  # Instala el teclado en pantalla
+else
+    echo "[SYS]: Disabling on-screen keyboard..."
+    #sudo apt remove -y onboard  # Desinstala el teclado en pantalla
+fi
+
+echo $value
