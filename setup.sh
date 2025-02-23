e6463031d2181e4b26b93af379b9868e5fc890d2
GustavoCruz <>
2025-02-23 18:16:48 +0100

    first commit

--- setup.sh
+++ setup.sh
@@ -2,9 +2,28 @@
 
 CONFIG_FILE="config"
 
-read -p "Inicializar la customizacion? (y/n): " enable_keyboard
-if [[ "$enable_keyboard" == "s" ]]; then
-    update_config "onscreen_keyboard" "enabled"
+# Modo debug: se activa si se pasa "y" como argumento
+debug_mode="n"
+if [[ "$1" == "y" ]]; then
+    debug_mode="y"
+    echo "[DEBUG]: Debug mode enabled."
+fi
+
+# Si el modo debug está activado, actualiza el repositorio si es un proyecto Git
+if [[ "$debug_mode" == "y" ]]; then
+    if [ -d ".git" ]; then
+        echo "[DEBUG]: Updating repository..."
+        git pull
+        echo "[SYS]: Repository updated successfully."
+    else
+        echo "[DEBUG]: No Git repository detected."
+    fi
+fi
+
+# Preguntar al usuario si desea inicializar la personalización
+read -p "Initialize customization? (y/n): " start_customize
+if [[ "$start_customize" == "y" ]]; then
+    echo "customized"
 else
-    update_config "onscreen_keyboard" "disabled"
+    echo "not customized"
 fi
